ca2d9fead4c14a1bcb7cb6a77be43b01296a5a60
VanTrung2000 <vantrungdeptrai2000@gmail.com>
2020-09-15 09:54:12 +0700

    Add project files.

---
+++ StaticProperty/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticProperty
+{
+    class Car
+    {
+        private string name;
+        private string engine;
+        public static int numberOfCars;
+        public Car(string name, string engine)
+        {
+            this.name = name;
+            this.engine = engine;
+            numberOfCars++;
+        }
+    }
+}
---
+++ StaticProperty/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StaticProperty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("Mazda 3", "Skyactiv 3");
+            Console.WriteLine(Car.numberOfCars);
+            Car car2 = new Car("Mazda 1", "Skyactiv 4");
+            Console.WriteLine(Car.numberOfCars);
+            Car car3 = new Car("Mazda 2", "Skyactiv 5");
+            Console.WriteLine(Car.numberOfCars);
+            Car car4 = new Car("Mazda 4", "Skyactiv 6");
+            Console.WriteLine(Car.numberOfCars);
+            Console.ReadLine();
+        }
+    }
+}
